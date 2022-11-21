59179008347f1bd509fdfd09c95d4fc310d683db
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2022-11-20 22:22:45 -0500

    Initial Commit

---
+++ project5-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project5-2
+//
+//  Created by Justine kenji Dela Cruz on 20/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
